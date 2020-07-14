2286bd057d5128ffad9264f491795fbe405f29f0
Tony Huang <tonyhuang@hotmail.co.uk>
2020-07-14 19:49:25 +0100

    Updated all CI to upload coverage to CodeCov.io

--- scripts/setupBackend.sh
+++ scripts/setupBackend.sh
@@ -2,4 +2,4 @@
 set -ev
 cd backend
 mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
-mvn test -Dtest="com.qa.tests.unit.**" && mvn test -Dtest="com.qa.tests.integration.**"
+mvn test -B -Dtest="com.qa.tests.unit.**" && mvn test -B -Dtest="com.qa.tests.integration.**"
